eb6d8c1d6d217079b95e21cbe9cede1ac7a8fb21
Mvoi <mvoifranklin@gmail.com>
2023-11-12 13:09:49 +0300

    Updated main.h - added headers

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef /*MAIN_H*/
 #define /*MAIN_H*/
 
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
 
 #endif /*MAIN_H*/
